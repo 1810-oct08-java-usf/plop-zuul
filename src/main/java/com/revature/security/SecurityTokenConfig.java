412aff613e6e9ae6b85811bf8681aa6510ebfe43
Caleb Massey <cabobmassey@gmail.com>
2018-12-07 16:49:19 -0500

    Fized CORS issues... again

--- src/main/java/com/revature/security/SecurityTokenConfig.java
+++ src/main/java/com/revature/security/SecurityTokenConfig.java
@@ -84,22 +84,6 @@ public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated();
 	}
 
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("OPTIONS");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("DELETE");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}
-
 	@Autowired
 	public void setJwtConfig(JwtConfig jwtConfig) {
 		this.jwtConfig = jwtConfig;
